aea181002075e381d99f2238502459c963827bba
Jose Loarca <joseloarca97@icloud.com>
2026-02-25 15:31:41 -0500

    Initial commit

---
+++ test/find-me-a-job-orchestrator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FindMeAJobOrchestrator from '../lib/find-me-a-job-orchestrator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/find-me-a-job-orchestrator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FindMeAJobOrchestrator.FindMeAJobOrchestratorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
